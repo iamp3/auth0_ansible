f48bc498f6120afe22778fc3db8a07c25907e698
Prisyazhnyuk Pavel <templar15.2012@gmail.com>
2019-07-05 19:29:03 +0300

    f

--- auth0_ext.py
+++ auth0_ext.py
@@ -7,47 +7,38 @@ def main():
         argument_spec = dict(
             token=dict(type='str', required=True),
             domain=dict(type='str', required=True),
-            action=dict(required=True, choises=['create_group', 'add_members']),
-            description=dict(type='str', required=True),
-            group=dict(type='str', required=True),
-            members=dict(required=False, type='list', default=[])
+            state=dict(required=False, choises=['present', 'absent'], default='present'),
+            groups=dict(type='str', required=True),
+            members=dict(type='str', required=True)
         )
     )
 
-    if module.params['action'] == 'create_group':
+    if module.params['state'] == 'present':
         create_group(module)
-    elif module.params['action'] == 'add_members':
-        add_members(module)
+    elif module.params['state'] == 'absent':
+        delete_group(module)
 
-def create_group(module):      
-    url = module.params['domain'] + "/groups"
-    payload = ('{ "name" : "' + str(module.params['group']) + '", "description": "' + str(module.params['description']) +'"}')
+def create_group(module):
+    g_url = module.params['domain'] + "/groups"
+    payload = ('{ "name" : "' + str(module.params['groups']) + '", "description": "' + str(module.params['groups']) +'"}')
 
     headers = {
         'Content-type': 'application/json',
         'authorization': module.params['token']
-    }    
+    }   
     
-    r = requests.request("POST", url, data=payload, headers=headers)    
-
-    if r.status_code == 200:
-        return module.exit_json(changed=False, meta=r.json())
+    rc = requests.request("POST", g_url, data=payload, headers=headers)
+    response = rc.json()
+    
+    if rc.status_code == 200:
+        m_payload = ('[ "' + str(module.params['members']) + '" ]')
+        m_url = module.params['domain'] + str("/groups/" + response['_id']) + "/members"
+        print(m_payload)
+        rm = requests.request("PATCH", m_url, data=m_payload, headers=headers)
     else:
-        return module.fail_json(msg=r.json())
+        return module.exit_json(changed=False)
 
-def add_members(module):      
-    url = module.params['domain'] + "/groups" + "id_" + "/members"
-    print(module.params['members'])
-    payload = ('['+ str(module.params['members']) +']')
-
-    headers = {
-        'Content-type': 'application/json',
-        'authorization': module.params['token']
-    }    
-    
-    r = requests.request("PATCH", url, data=payload, headers=headers)    
-
-    return module.fail_json(msg=r.json())
+    return module.exit_json(msg=rc.json())
 
 from ansible.module_utils.basic import *
 from ansible.module_utils.urls import *
